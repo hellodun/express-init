39aa2a5851b15aa7e640728ee992b22f87513b13
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-17 18:02:19 +0300

    download file from response

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.download("C:/Users/Dunco/Downloads/condo interior.jpg");
+})
+
 app.listen(8080, console.log("App listening on port 8080"));
