b175de07f20a33a2fbe550d8bfab20fd47efb472
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-17 18:29:58 +0300

    Set up Routing with express

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", {
     text: "From the server"
-  })
-})
+  });
+});
+
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
 
 app.listen(8080, console.log("App listening on port 8080"));
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Users List");
+});
+
+router.get("/new", (req, res) => {
+  res.send("New Users Form");
+});
+
+
+module.exports = router;
