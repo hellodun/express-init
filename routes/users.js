8f755365b3262dbc79319408c624792778c13a1f
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-17 18:58:28 +0300

    Chaining multiple methods with the same params

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,40 @@ router.get("/new", (req, res) => {
   res.send("New Users Form");
 });
 
+// NOTE: All dynamic routes should come after the static routes
+// Dynamic routes are those with format e.g. /:id
+// Get data
+router.get("/:id", (req, res) => {
+  res.send(`Get data for user: ${req.params.id}`);
+});
+
+// update data
+router.put("/:id", (req, res) => {
+  res.send(`Update data for user: ${req.params.id}`);
+});
+
+// Delete data
+router.delete("/:id", (req, res) => {
+  res.send(`Delete data for user: ${req.params.id}`);
+});
+
+// ----------- The above three routes are the same are the following ------------
+
+/*
+router.route("/:id").get((req, res) => {
+  res.send(`Get data for user: ${req.params.id}`)
+})
+  .put((req, res) => {
+    res.send(`Update data for user: ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    res.send(`Delete data for user: ${req.params.id}`);
+  });
+
+*/
+
+// This is because the 3 functions have identical structure and the difference is the HTTP method, i.e. get, put, delete.
+
+
 
 module.exports = router;
