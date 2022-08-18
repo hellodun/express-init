ddf72bfaa6512e8e6ae436c7a8ce5678abde26dc
Duncan Gatuthu <dgatuthu@gmail.com>
2022-08-18 08:52:36 +0300

    Implemented a simple middleware

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use(logger); // By specifying our logger here, the logger will be used by all routes
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+// This implementation of logger will only be used by the home route i.e /, provided logger isn't specified elsewhere
+app.get("/", logger, (req, res) => {
   res.render("index", {
     text: "From the server"
   });
@@ -13,6 +16,12 @@ app.get("/", (req, res) => {
 
 const userRouter = require("./routes/users");
 app.use("/users", userRouter);
+
+// Custom middleware to log the visited route
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 
 app.listen(8080, console.log("App listening on port 8080"));
